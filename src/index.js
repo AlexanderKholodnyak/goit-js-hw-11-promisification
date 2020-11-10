1d6274f5e232e274783b0cba618eb17cacdb6942
Alexander Kholodnyak <kholodnyak.a@gmail.com>
2020-11-10 18:12:07 +0200

    123

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './styles.css';
 
-import * as taskOne from './js/task-1.js';
-import * as taskTwo from './js/task-2.js';
-import * as taskThree from './js/task-3.js';
+import './js/task-1.js';
+import './js/task-2.js';
+import './js/task-3.js';
